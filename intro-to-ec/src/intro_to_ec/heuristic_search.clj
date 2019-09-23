0c700c94cd323744b6e07f25c0269847d50f2867
Nick Plucker <pluck011@morris.umn.edu>
2019-09-23 15:21:08 -0500

    Implement a-star search

--- intro-to-ec/src/intro_to_ec/heuristic_search.clj
+++ intro-to-ec/src/intro_to_ec/heuristic_search.clj
@@ -28,28 +28,51 @@
   [b]
   (+ (Math/abs(- 0 (get b 0))) (Math/abs(- 0 (get b 1)))))
 
-(defn search
+(defn heuristic-search
   [{:keys [get-next-node add-children]}
    {:keys [goal? make-children]}
-   start-node max-calls]
+   start-node
+   max-calls]
   (loop [frontier [start-node]
          came-from {start-node :start-node}
          num-calls 0]
-    ; (println num-calls ": " frontier)
-    ; (println came-from)
-    (println "Frontier: " frontier)
-    ;(println "Start Node: " start-node)
+    (println "Frontier: " frontier " Num Calls: " num-calls)
     (let [current-node (get-next-node frontier)]
-    ;(println "Current Node: " current-node)
       (cond
         (goal? current-node) (generate-path came-from current-node)
         (= num-calls max-calls) :max-calls-reached
         :else
         (let [kids (remove-previous-states
                     (make-children current-node) frontier (keys came-from))]
-                    ;(println "Kids: " kids)
+                    (println "Kids: " kids " Num Calls: " num-calls)
           (let [priority-frontier (reverse (vec
                   (into (pq/priority-queue #(manhattan-distance-heuristic %)) kids)))]
-          (recur priority-frontier
-           (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
-           (inc num-calls))))))))
+            (recur priority-frontier
+             (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
+             (inc num-calls))))))))
+
+(defn a-star-search
+ [{:keys [get-next-node add-children]}
+  {:keys [goal? make-children]}
+  start-node
+  max-calls]
+ (loop [frontier [start-node]
+        came-from {start-node :start-node}
+        cost-so-far {start-node 0}
+        num-calls 0]
+   (println "Frontier: " frontier " Num Calls: " num-calls)
+   (let [current-node (get-next-node frontier)]
+     (cond
+       (goal? current-node) (generate-path came-from current-node)
+       (= num-calls max-calls) :max-calls-reached
+       :else
+       (let [kids (remove-previous-states
+                   (make-children current-node) frontier (keys came-from))]
+                   (println "Kids: " kids " Num Calls: " num-calls)
+         (let [updated-cost (+ 1 (get cost-so-far current-node))]
+           (let [priority-frontier (reverse (vec
+                   (into (pq/priority-queue #(+ (manhattan-distance-heuristic %) updated-cost)) kids)))]
+             (recur priority-frontier
+              (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
+              (reduce (fn [cost child] (assoc cost child updated-cost)) cost-so-far kids)
+              (inc num-calls)))))))))
